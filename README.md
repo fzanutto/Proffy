cdaf90911b9528bfca338b8e62f2fa07e5b7d6d4
Fernando Zanutto <ferzanutto1999@gmail.com>
2020-08-04 18:10:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proffy
+ Projeto para conectar alunos e professores. Desenvolvido durante a Next Level Week #2 da Rockseat
---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Landing from './pages/Landing'
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
